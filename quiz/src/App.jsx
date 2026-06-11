2b257e524d905ebd215402a291a49cd8c1db0fbd
Seung-min <peppyksm@gmail.com>
2026-06-11 14:56:07 +0900

    Quiz0 리액트 프로젝트 생성 관련

---
+++ quiz/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <div>
+        <h1>React 프로젝트</h1>
+        <br /><br />
+        <h3>김승민</h3>
+      </div>
+    </div>
+  )
+}
+
+export default App
