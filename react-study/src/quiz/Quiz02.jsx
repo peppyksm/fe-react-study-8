0193743381cd027b3617ae2f9e30576e150ee78c
Seung-min <peppyksm@gmail.com>
2026-06-15 11:01:45 +0900

    리액트 Quiz02 바닐라코드에서 수정

--- react-study/src/quiz/Quiz02.jsx
+++ react-study/src/quiz/Quiz02.jsx
@@ -1,30 +1,24 @@
+import { useState } from "react";
+
 function Quiz02() {
-    
-    let count = 1;
+
+    let [count, setCount] = useState(0);
+    let text = ['하나', '둘', '셋'];
+
+    const countBtn = (() => {
+        if (count == 2) {
+            setCount(0);
+        } else {
+            setCount(count + 1);
+        }
+    })
+
 
     return (
         <div className="main">
             <div className="container">
-                <p id="text">하나</p>
-                <button id="btn" onClick={()=>{
-                    const text = document.getElementById('text');
-
-                    if(count == 3){
-                        count = 0;
-                    }
-                    count++;
-
-                    if(count == 1){
-                        text.textContent = "하나";
-                    }else if(count == 2){
-                        text.textContent = "둘";
-                    }else if(count == 3){
-                        text.textContent = "셋";
-                    }
-                    console.log(count)
-
-
-                }}>변경버튼</button>
+                <p>{text[count]}</p>
+                <button onClick={countBtn}>변경버튼</button>
             </div>
 
         </div>
